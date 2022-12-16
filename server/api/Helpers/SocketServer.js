b47720d8ed2d8e4e373946a40ce5078e0a405caf
hakandincturk <hakan.dincturkk@gmail.com>
2022-12-16 17:38:02 +0300

    new conversationId structure ok

--- server/api/Helpers/SocketServer.js
+++ server/api/Helpers/SocketServer.js
@@ -20,12 +20,15 @@ class SocketServer {
 			const id = socket.handshake.query.id;
 			socket.join(id);
     
-			socket.on('send-message', ({ recipients, text }) => {
+			socket.on('send-message', ({ conversationId, recipients, text }) => {
 				recipients.forEach(recipient => {
 					const newRecipients = recipients.filter(r => r !== recipient);
 					newRecipients.push(id);
 					socket.broadcast.to(recipient).emit('receive-message', {
-						recipients: newRecipients, sender: id, text
+						conversationId,
+						recipients: newRecipients, 
+						sender: id, 
+						text
 					});
 				});
 			});
